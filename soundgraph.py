36a3548846aa462dcf7b2ce5934c4d6d27cb9c01
mikebito <30741054+mikebito@users.noreply.github.com>
2020-09-23 17:59:30 +0900

    aaa

--- soundgraph.py
+++ soundgraph.py
@@ -3,13 +3,13 @@ import numpy as np                  #計算用
 import matplotlib.pyplot as plt     #グラフ化用
 
 #設定
-chunk = 2048             #音声データメモリーサイズ指定
-FORMAT = pyaudio.paInt16        #16進数に指定
+chunk = 1024         #音声データメモリーサイズ指定
+FORMAT = pyaudio.paInt32        #16進数に指定
 CHANNELS = 1                    #モノラルに指定
-RATE = 96000                     #サンプリング速度-サンプリング周波数(、1秒間に実行する標本化（サンプリング）処理の回数のこと)
+RATE = 95232                     #サンプリング速度-サンプリング周波数(、1秒間に実行する標本化（サンプリング）処理の回数のこと)
 T = 1/RATE
 RECORD_SECONDS = 1              #３秒録音
-t = np.arange(0, chunk*T, T) #時間軸が1/2倍違う
+t = np.arange(0, RATE*T, T) #時間軸が1/2倍違う
 
 p = pyaudio.PyAudio()           #!!!要調べ！！！
 
@@ -34,26 +34,29 @@ stream.close()
 p.terminate()
 
  #!!!要調べ！！！
-data = b"".join(all)
-result = np.frombuffer(data,dtype="int32") / float (2**15)
+data2 = b"".join(all)
+result = np.frombuffer(data2,dtype="int32") / float (2**15)
+
 with open ('file1.txt', 'w') as f:
     np.set_printoptions(threshold=np.inf)
+    allen = len(all)
+    print(allen)
     print(result, file=ｆ)
     cal = len(result)
     print(cal)
 
-result2 = np.where((result > -100) & (result < 100) , 0 , result) 
+result2 = np.where((result > -70) & (result < 70) , 0 , result) 
 
 
 F = np.fft.fft(result2) 
 # FFTの複素数結果を絶対に変換
 F_abs = np.abs(F)
 # 振幅をもとの信号に揃える
-F_abs_amp = F_abs / chunk * 2 # 交流成分はデータ数で割って2倍
+F_abs_amp = F_abs / RATE * 2 # 交流成分はデータ数で割って2倍
 F_abs_amp[0] = F_abs_amp[0] / 2 # 直流成分（今回は扱わないけど）は2倍不要
 
 # 周波数軸のデータ作成
-fq = np.linspace(0, 1.0/T, chunk) # 周波数軸　linspace(開始,終了,分割数)
+fq = np.linspace(0, 1.0/T, RATE) # 周波数軸　linspace(開始,終了,分割数)
 
 # グラフ表示
 fig = plt.figure(figsize=(12, 4))
@@ -68,5 +71,5 @@ plt.plot(t,result2)
 ax2 = fig.add_subplot(122)
 plt.xlabel('freqency(Hz)', fontsize=14)
 plt.ylabel('amplitude', fontsize=14)
-plt.plot(fq[:int(chunk/2)+1], F_abs_amp[:int(chunk/2)+1]) # ナイキスト定数まで表示
+plt.plot(fq[:int(RATE/2)+1], F_abs_amp[:int(RATE/2)+1]) # ナイキスト定数まで表示
 plt.show()
