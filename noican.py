91db7dfb92ee9816f81dc0b979cc977f150bf27a
mikebito <miki0302sin@outlook.com>
2021-07-16 18:40:38 +0900

    sinchoku

---
+++ noican.py
@@ -0,0 +1,52 @@
+import pyaudio                      #録音用    
+import numpy as np                  #計算用
+import matplotlib.pyplot as plt     #グラフ化用
+import itertools
+import struct
+import wave
+
+
+
+
+#設定
+chunk = 2 ** 10        #音声データメモリーサイズ指定
+FORMAT = pyaudio.paInt32        #16進数に指定
+CHANNELS = 1                    #モノラルに指定
+RATE = 43008 #サンプリング速度-サンプリング周波数(、1秒間に実行する標本化（サンプリング）処理の回数のこと)
+T = 1/RATE
+RECORD_SECONDS = 3        #6秒録音
+inputIndexNumber = int(6)
+outputIndexNumber = int(9)
+t = np.arange(0, RATE*T*RECORD_SECONDS, T) #時間軸が1/2倍違う
+
+p = pyaudio.PyAudio()          
+
+
+stream = p.open(format = FORMAT,
+                channels = CHANNELS,
+                rate = RATE,
+                input = True,
+                input_device_index = inputIndexNumber,
+                output_device_index = outputIndexNumber,
+                output = True,
+                frames_per_buffer = chunk
+)
+all = []
+
+def audio_trans(data):
+    all.append(data)
+    data2 = b"".join(all) #b""
+    result = np.frombuffer(data2,dtype="int32") / float (2**15)
+    result = result * -1
+    return result
+
+while stream.is_active():
+    data = stream.read(chunk,exception_on_overflow = False)
+    data = audio_trans(data)
+    output = stream.write(data.astype(np.float32).tobytes())
+	
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+print ("Stop Streaming")
